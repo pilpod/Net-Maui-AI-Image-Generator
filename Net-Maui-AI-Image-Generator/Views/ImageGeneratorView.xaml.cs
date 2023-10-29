d8cbc6f2daaa250a34d1ffadaffc26c3a40abe63
Giacomo C <giacomo.corso@gmail.com>
2023-10-29 18:35:15 +0100

    Image generator view btn finish animation done and image animation done. pending spent time

--- Net-Maui-AI-Image-Generator/Views/ImageGeneratorView.xaml.cs
+++ Net-Maui-AI-Image-Generator/Views/ImageGeneratorView.xaml.cs
@@ -6,4 +6,29 @@ public partial class ImageGeneratorView : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void btnFinish_Clicked(object sender, EventArgs e)
+	{
+		StopGeneration();
+	}
+
+	private async void StopGeneration()
+	{
+		await Task.Delay(2000);
+
+		lottie.IsAnimationEnabled = false;
+		lottie.IsVisible = false;
+		imageBorder.IsVisible = true;
+		imageAnimation.IsVisible = true;
+
+		await Task.WhenAny(
+			imageAnimation.ScaleTo(1.1, 1000),
+			imageAnimation.FadeTo(0, 1000),
+			borderTime.ScaleTo(1, 1000),
+			borderTime.FadeTo(1, 1000)
+			);
+
+		await borderTime.FadeTo(0, 300);
+		await btnFinish.ScaleTo(1, 1000);
+	}
 }
